ab70aec47d0a8b418ab3e45652349e407c467364
taofik-adeniyi <bidemi64@gmail.com>
2020-07-28 11:10:27 +0100

    error message for wrong answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,11 +3,15 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
   constructor(props) {
-    super()
+    super(props)
+    this.state = { incorrectAnswer: false }
   }
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrectAnswer: false })
       this.props.showNextQuestionHandler()
+    }else {
+      this.setState({ incorrectAnswer: true })
     }
   }
   render() {
@@ -26,6 +30,7 @@ class QuizQuestion extends Component {
           }
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p>: null}
       </main>
     )
   }
