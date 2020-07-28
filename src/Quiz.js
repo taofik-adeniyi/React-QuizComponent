724f06a47519cbd7804df924933eedd12a07d212
taofik-adeniyi <bidemi64@gmail.com>
2020-07-28 11:38:07 +0100

    reset button

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,10 @@ constructor(props) {
   super(props)
   this.state = {quiz_position: 1}
 }
+
+handleResetClick() {
+  this.setState({ quiz_position: 1 })
+}
 showNextQuestion() {
   this.setState((state) => {
     return { quiz_position: state.quiz_position +1 }
@@ -19,7 +23,7 @@ render() {
   const isQuizEnd = ( (this.state.quiz_position -1) === quizData.quiz_questions.length )
   return (
     <div>
-      {isQuizEnd ? <QuizEnd /> :
+      {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this) } /> :
       <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1 ]}
       showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
     </div>
